8ce20114673a4958aec61413940168c844c26054
mertziya <mert7ziya@gmail.com>
2025-04-03 12:15:44 +0300

    initial commit

---
+++ ReadingAppTestCase/ReadingAppTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingAppTestCaseApp.swift
+//  ReadingAppTestCase
+//
+//  Created by Mert Ziya on 3.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReadingAppTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
